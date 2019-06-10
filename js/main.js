2e09b272c1a2412791082f8ed748c99e109c8f87
Claude Ayitey <clauditey@gmail.com>
2019-06-10 15:10:09 +0000

    language toggle working now.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $('#tab-en').on('click', function(){
     console.log('english clicked');
     $('.episode--grid').hide();
     // remove class on link
-    $('.episode--grid__en').show();
+    $('.episode--grid__en').show().fadeTo();
     // add class on new link
     // show my id
 });
@@ -12,7 +12,7 @@ $('#tab-fr').on('click', function(){
     console.log('french clicked');
     $('.episode--grid').hide();
     // remove class on link
-    $('.episode--grid__fr').show();
+    $('.episode--grid__fr').show().fadeTo();
     // add class on new link
     
     // show my id
