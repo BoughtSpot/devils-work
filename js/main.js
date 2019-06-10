fe259af738dfbf17c2e3940799a9f9a280df53c8
Claude Ayitey <clauditey@gmail.com>
2019-06-10 05:56:52 +0000

    Video popup working now, thanks to Magnific popup

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$('.episode--grid__en').magnificPopup({
+    items: [
+        {
+            delegate: 'a',
+            src: 'https://vimeo.com/40004005',
+            type: 'iframe'
+        },
+        {
+            delegate: 'b',
+            src: 'https://vimeo.com/25037336',
+            type: 'iframe'
+        },
+        {
+            delegate: 'c',
+            src: 'https://vimeo.com/57486433',
+            type: 'iframe'
+        }
+    ]
+});
+
+$('.episode--grid__fr').magnificPopup({
+    items: [
+        {
+            delegate: 'a',
+            src: 'https://vimeo.com/99651914',
+            type: 'iframe'
+        },
+        {
+            delegate: 'b',
+            src: 'https://vimeo.com/99647025',
+            type: 'iframe'
+        },
+        {
+            delegate: 'c',
+            src: 'https://vimeo.com/96715060',
+            type: 'iframe'
+        }
+    ],
+    gallery: {
+        enabled: true
+    },
+    type: 'iframe'
+});
